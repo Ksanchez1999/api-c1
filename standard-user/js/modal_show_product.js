602ab705868af2c87206a979073646e5e4409358
Kevin <kevinanthony525gmail.com>
2026-04-22 19:33:58 -0400

    Corrección N°9 a proyecto final

--- standard-user/js/modal_show_product.js
+++ standard-user/js/modal_show_product.js
@@ -42,14 +42,9 @@ export function showProduct(data) {
   imgContainer.className = "imgContainer";
   feedbackModalBody.appendChild(imgContainer);
 
-  imgContainer.innerHTML = `
-      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M20.25 18.375L12 22.5L3.75 18.375V9.375L12 5.25L20.25 9.375V18.375Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M3.75 9.375L12 13.5L20.25 9.375" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M12 22.5V13.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M12 5.25V2" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-      </svg>
-    `;
-
+  const imgIco = document.createElement('img');
+  imgIco.src = '../img/check-icon.png';
+  imgIco.alt = 'Descripción de la imagen';
+  imgContainer.appendChild(imgIco);
 }
 
